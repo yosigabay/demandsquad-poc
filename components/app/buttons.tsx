c3df83530071fd5b31dade7d754dfe3421ab99b3
yosigabay <ygabay@Yosis-MacBook-Air.local>
2024-05-28 22:33:21 -0700

    minor

--- components/app/buttons.tsx
+++ components/app/buttons.tsx
@@ -7,7 +7,7 @@ export function CreateProjectBtn() {
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
       href="/app/create"
     >
-      <span className="hidden md:block">Create Project</span>{' '}
+      <span className="hidden md:block">Create Project</span>{" "}
       <PlusIcon className="h-5 md:ml-4" />
     </Link>
   );
@@ -16,8 +16,8 @@ export function CreateProjectBtn() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
+      href={`/dashboard/invoices/${id}/edit`}
     >
       <PencilIcon className="w-5" />
     </Link>
@@ -26,7 +26,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   //const deleteInvoiceWithId = deleteInvoice.bind(null, id);
- 
+
   return (
     <form /*action={deleteInvoiceWithId}>*/>
       <button className="rounded-md border p-2 hover:bg-gray-100">
