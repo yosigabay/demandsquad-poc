168ba228685e52496a1b221cba69baef8474c695
yosigabay <ygabay@Yosis-MacBook-Air.local>
2024-05-29 00:29:28 -0700

    fix prerender error

--- components/site/navbar.tsx
+++ components/site/navbar.tsx
@@ -10,13 +10,7 @@ import { Button } from "@nextui-org/button";
 import { button as buttonStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -29,7 +23,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">DemandSquasd</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -49,44 +43,43 @@ export const Navbar = () => {
           ))}
         </ul>
       </NavbarContent>
-      <ClerkProvider signInFallbackRedirectUrl="/app">
-        <NavbarContent
-          className="hidden sm:flex basis-1/5 sm:basis-full"
-          justify="end"
-        >
-          <SignedOut>
-            <NavbarItem className="hidden md:flex">
-              <SignInButton>
-                <Button
-                  className={buttonStyles({
-                    color: "primary",
-                    radius: "full",
-                    variant: "shadow",
-                  })}
-                >
-                  Sign In
-                </Button>
-              </SignInButton>
-            </NavbarItem>
-            <NavbarItem>
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <SignedOut>
+          <NavbarItem className="hidden md:flex">
+            <SignInButton>
               <Button
                 className={buttonStyles({
+                  color: "primary",
                   radius: "full",
-                  variant: "bordered",
+                  variant: "shadow",
                 })}
               >
-                Sign Up
+                Sign In
               </Button>
-            </NavbarItem>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <NavbarItem className="hidden sm:flex gap-2">
-            <ThemeSwitch />
+            </SignInButton>
           </NavbarItem>
-        </NavbarContent>
-      </ClerkProvider>
+          <NavbarItem>
+            <Button
+              className={buttonStyles({
+                radius: "full",
+                variant: "bordered",
+              })}
+            >
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <NavbarItem className="hidden sm:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+      </NavbarContent>
+
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <ThemeSwitch />
         <NavbarMenuToggle />
